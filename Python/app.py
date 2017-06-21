0c5cca1a460951abe0922c7083ec3d381e0c36b4
GeekFox <rafael.brasileiro@gmail.com>
2017-06-21 17:53:39 -0300

    First version of webservice

--- Python/app.py
+++ Python/app.py
@@ -1,11 +1,34 @@
 #!flask/bin/python
-from flask import Flask
+from flask import Flask, send_from_directory
+from picturedata.PictureData import PictureData
 
 app = Flask(__name__)
+
+picturedata = PictureData()
 
 @app.route("/")
 def index():
     return "Hello world!"
+
+@app.route("/images/<path:path>")
+def send_images(path):
+    return send_from_directory('images', path)
+
+@app.route("/socialimage/api/v1.0/images", methods=['GET'])
+def get_images():
+    return picturedata.get_data()
+
+@app.route("/socialimage/api/v1.0/images/<int:image_id>", methods=['GET'])
+def get_image(image_id):
+    return picturedata.get_image(image_id)
+
+@app.route("/socialimage/api/v1.0/images", methods=['POST'])
+def new_image():
+    return picturedata.new_image()
+
+@app.route("/socialimage/api/v1.0/images/<int:image_id>", methods=['PUT'])
+def add_description(image_id):
+    return picturedata.add_description(image_id)
 
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ Python/picturedata/PictureData.py
@@ -0,0 +1,54 @@
+#!usr/bin/python
+from flask import jsonify, abort, request
+import os
+
+class PictureData:
+    data = []
+
+    def __init__(self):
+        #load data from filesystem
+        files = os.listdir('images')
+        for f in files:
+            file_data = f.split('.')
+            image = {
+                'id': file_data[0],
+                'image': '/images/{0}'.format(f),
+                'extension': file_data[1],
+                'description': ''
+            }
+            self.data.append(image)
+
+
+    def get_data(self):
+        return jsonify({'data': self.data})
+
+    def get_image(self, image_id):
+        image = [image for image in self.data if image['id'] == image_id]
+        if len(image) == 0:
+            abort(404)
+        return jsonify({'image':image[0]})
+
+    def new_image(self):
+        if not request.json or not 'image' in request.json:
+            abort(400)
+
+        image_data = request.json.get('image')
+        #save image
+        index = self.data[-1]['id'] + 1
+        with open('images/{0}.{1}'.format(index, request.json.get('extension')), 'wb') as fh:
+            fh.write(image_data.decode('base64'))
+        image = {
+            'id': self.data[-1]['id'] + 1,
+            'extension': request.json.get('extension'),
+            'image': 'images/{0}.{1}'.format(index, request.json.get('extension')),
+            'description': ''
+        }
+        self.data.append(image)
+        return jsonify({'image': image}), 201
+
+    def add_description(self, image_id):
+        if not request.json or not 'description' in request.json:
+            abort(400)
+        with open('descriptions/{0}.txt'.format(self.data[-1]['id'] + 1), 'a') as fh:
+            fh.write('{0}\n'.format(request.json.get('description')))
+        return jsonify({'result': 'Description added'})
