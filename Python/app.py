b140f2f44f68d433102f6ec4104b575f1ddccb72
GeekFox <rafael.brasileiro@gmail.com>
2017-06-28 16:48:56 -0300

    Changes in python app to allow access from network, more work on android app

--- Python/app.py
+++ Python/app.py
@@ -14,21 +14,21 @@ def index():
 def send_images(path):
     return send_from_directory('images', path)
 
-@app.route("/socialimage/api/v1.0/images", methods=['GET'])
+@app.route("/socialpicture/api/v1.0/images", methods=['GET'])
 def get_images():
     return picturedata.get_data()
 
-@app.route("/socialimage/api/v1.0/images/<int:image_id>", methods=['GET'])
+@app.route("/socialpicture/api/v1.0/images/<int:image_id>", methods=['GET'])
 def get_image(image_id):
     return picturedata.get_image(image_id)
 
-@app.route("/socialimage/api/v1.0/images", methods=['POST'])
+@app.route("/socialpicture/api/v1.0/images", methods=['POST'])
 def new_image():
     return picturedata.new_image()
 
-@app.route("/socialimage/api/v1.0/images/<int:image_id>", methods=['PUT'])
+@app.route("/socialpicture/api/v1.0/images/<int:image_id>", methods=['PUT'])
 def add_description(image_id):
     return picturedata.add_description(image_id)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0')
