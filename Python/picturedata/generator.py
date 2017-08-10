3165064111eacd164bf035caf91ffb625a15ba27
GeekFox <rafael.brasileiro@gmail.com>
2017-08-10 19:25:31 -0300

    modification to load flickr8k

--- Python/picturedata/generator.py
+++ Python/picturedata/generator.py
@@ -12,9 +12,9 @@ class DataGenerator:
         self.index_word = None
         self.word_index = None
         self.total_samples = None
-        self.images = pickle.load(open('image_features.p', 'rb'))
-        data = pd.read_csv('data/training.csv')
-        self.captions = data['caption']
+        self.features = pickle.load(open('image_features.p', 'rb'))
+        self.data = pd.read_csv('data/flickr8k_train.csv', delimiter=',')
+
         self.initValues()
 
     def initValues(self):
@@ -38,12 +38,16 @@ class DataGenerator:
             self.index_word[i]=word
 
         max_len = 0
-        for caption in self.captions:
-            if(len(caption.split()) > max_len):
+        for caption in self.data:
+            if(len(caption[1].split()) > max_len):
                 max_len = len(caption.split())
         self.max_cap_len = max_len
         print "Vocabulary size: "+str(self.vocab_size)
         print "Maximum caption length: "+str(self.max_cap_len)
+
+        self.images = []
+        for caption in self.data:
+            self.images.append(data[caption[0]])
 
     def generate(self, batch_size=32):
         partial_caps = []
@@ -55,15 +59,15 @@ class DataGenerator:
         total_count = 1
         while 1:
             image_counter = -1
-            for text in self.captions:
+            for text in self.data:
                 image_counter += 1
                 current_image = self.images[image_counter]
-                for i in range(len(text.split())-1):
+                for i in range(len(text[1].split())-1):
                     total_count += 1
-                    partial = [self.word_index[txt] for txt in text.split()[:i+1]]
+                    partial = [self.word_index[txt] for txt in text[1].split()[:i+1]]
                     partial_caps.append(partial)
                     next = np.zeros(self.vocab_size)
-                    next[self.word_index[text.split()[i+1]]] = 1
+                    next[self.word_index[text[1].split()[i+1]]] = 1
                     next_words.append(next)
                     imgs.append(current_image)
 
