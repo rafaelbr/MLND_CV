7ec7932def01e38d9acfe56d3a86463355a16c27
GeekFox <rafael.brasileiro@gmail.com>
2017-09-18 17:06:17 -0300

    Convert PictureData to Singleton

--- Python/picturedata/PictureData.py
+++ Python/picturedata/PictureData.py
@@ -5,7 +5,15 @@ import json
 import os
 import threading
 
+class Singleton(type):
+    _instances = {}
+    def __call__(cls, *args, **kwargs):
+        if cls not in cls._instances:
+            cls._instances[cls] = super(Singleton, cls).__call__(*args, **kwargs)
+        return cls._instances[cls]
+
 class PictureData:
+    __metaclass__ = Singleton
     data = []
 
     def __init__(self):
